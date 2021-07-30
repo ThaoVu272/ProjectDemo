2100c6ad65c5de16744a1efe73b3a9db72f1dfbc
Admin <Admin@DESKTOP-171AVQP>
2021-07-30 14:46:18 +0700

    Add project files.

---
+++ BlazorProject/Pages/StudentList.razor.cs
@@ -0,0 +1,127 @@
+﻿using BlazorProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net;
+
+namespace BlazorProject.Pages
+{
+    public class StudentListBase : ComponentBase
+    {
+        
+        public IEnumerable<Student> Students { get; set; } = new List<Student>();
+        //public IHttpClientFactory ClientFactory;
+        public HttpClient http;
+      
+        public bool getBranchesError;
+        public bool shouldRender;
+       // public IHttpClientFactory _httpClient { get; set; };
+
+        //private void SetupClient(System.Net.Http.HttpClient client, string contentType)
+        //{
+        //    client.BaseAddress = new Uri(_httpClient);
+        //    client.DefaultRequestHeaders.Accept.Clear();
+        //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(contentType));
+        //}
+        protected override async Task OnInitializedAsync()
+        {
+            //Student = GetStudents();
+            //Students = await http.GetAsync<IEnumerable<Student>>("https://localhost:44325/api/Students");
+            //var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:44325/api/Students");
+            //try
+            //{
+            //    var serviceProvider = new ServiceCollection().AddHttpClient().BuildServiceProvider();
+            //    var httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
+            //    var client = httpClientFactory.CreateClient();
+            //    var response = await client.SendAsync(request);
+            //    if (response.IsSuccessStatusCode)
+            //    {
+            //        using var responseStream = await response.Content.ReadAsStreamAsync();
+            //        Students = await JsonSerializer.DeserializeAsync<List<Student>>(responseStream);
+            //    }
+            //    else
+            //    {
+            //        getBranchesError = true;
+            //    }
+
+            //    shouldRender = true;
+            //}
+            //catch(Exception ex)
+            //{
+            //    getBranchesError = true;
+            //    shouldRender = false;
+            //}
+            // string baseUrl = "http://localhost:44325";
+            var serviceProvider = new ServiceCollection().AddHttpClient().BuildServiceProvider();
+            var httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            var client = httpClientFactory.CreateClient("ResAPI");
+            client.Timeout = TimeSpan.FromMinutes(5);
+           
+
+            // client.BaseAddress = new Uri(baseUrl);
+            //var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+            //client.DefaultRequestHeaders.Accept.Add(contentType);
+            var message = new HttpRequestMessage();
+            message.Headers.Add("Accept", "application/json");
+            message.RequestUri = new Uri("http://localhost:44325/api/Students");
+            client.DefaultRequestHeaders.Clear();
+            
+            HttpResponseMessage apiResponse = null;
+            //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer");
+            try
+            {
+
+                apiResponse = await client.SendAsync(message);
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                //if (response.IsSuccessStatusCode)
+                //{
+                //    var stringData = response.Content.ReadAsStringAsync().Result;
+                   Students = JsonConvert.DeserializeObject<IEnumerable<Student>>(apiContent);
+                //}
+                //else
+                //{
+                //    Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                //    getBranchesError = true;
+                //    shouldRender = false;
+                //}
+
+            }
+        catch(Exception ex)
+            {
+
+            }
+            
+
+        }
+
+        async Task<IEnumerable<Student>> GetUsersAsync()
+        {
+            HttpClient client = new HttpClient();
+            List<Student> users = new List<Student>();
+            var response = client.GetAsync("http://localhost:44325/api/Students/GetStudents").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var stringData = response.Content.ReadAsStringAsync().Result;
+                Students = JsonConvert.DeserializeObject<IEnumerable<Student>>(stringData);
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                getBranchesError = true;
+                shouldRender = false;
+            }
+            return users;
+        }
+
+        private async Task SaveStudent()
+        {
+
+        }
+    }
+}
---
+++ CoreApiProject/Controllers/StudentsController.cs
@@ -0,0 +1,117 @@
+﻿using CoreApiProject.Models;
+using Dapper.Oracle;
+using Microsoft.AspNetCore.Mvc;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OracleClient;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        List<Student> _oStudent = new List<Student>();
+        public StudentsController()
+        {
+            //_oStudent = new List<Student>();
+            //for(int i=1; i <= 9; i++)
+            //{
+            //    _oStudent.Add(new Student()
+            //    {
+            //        StudentId = i,
+            //        Name ="Student"+i,
+            //        Roll = "100" +i
+            //    }) ;
+            //}
+            _oStudent = GetStudents();
+        }
+        // GET: api/<Students>
+        [HttpGet]
+        public List<Student> GetStudents()
+       {
+            DataTable dtEmp = new DataTable();
+            using (OracleConnection objConn = new OracleConnection("Data Source=THAODB; User ID=student; Password=1234"))
+            {
+                OracleDataAdapter objAdapter = new OracleDataAdapter();
+                OracleCommand objCmd = new OracleCommand();
+                objCmd.Connection = objConn;
+                objCmd.CommandText = "students_get";
+                objCmd.CommandType = CommandType.StoredProcedure;
+                objCmd.Parameters.Add("studentid", OracleDbType.Int32).Value = -1;
+                objCmd.Parameters.Add("rs", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
+                objAdapter.SelectCommand = objCmd;
+                try
+                {
+                    objConn.Open();
+                    objAdapter.Fill(dtEmp);
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine("Exception: {0}", ex.ToString());
+                }
+                objConn.Close();
+            }
+            List<Student> studentList = new List<Student>();
+            for (int i = 0; i < dtEmp.Rows.Count; i++)
+            {
+                Student student = new Student();
+                student.StudentId = Convert.ToInt32(dtEmp.Rows[i]["studentId"]);
+                student.Name = dtEmp.Rows[i]["name"].ToString();
+                student.Roll = dtEmp.Rows[i]["roll"].ToString();
+                studentList.Add(student);
+            }
+            return studentList;
+        }
+
+
+
+        // POST api/<Student>
+        [HttpPost]
+        public List<Student> Post([FromBody] Student oStudent)
+        {
+            using (OracleConnection objConn = new OracleConnection("Data Source=THAODB; User ID=student; Password=1234"))
+            {
+                OracleDataAdapter objAdapter = new OracleDataAdapter();
+                OracleCommand objCmd = new OracleCommand();
+                objCmd.Connection = objConn;
+                objCmd.CommandText = "students_i";
+                objCmd.CommandType = CommandType.StoredProcedure;
+                objCmd.Parameters.Add("p_name", OracleDbType.NVarchar2,255).Value = "Studentx";
+                objCmd.Parameters.Add("rs", OracleDbType.RefCursor).Direction = ParameterDirection.Output;
+                objAdapter.InsertCommand = objCmd;
+                try
+                {
+                    objConn.Open();
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine("Exception: {0}", ex.ToString());
+                }
+                objConn.Close();
+            }
+            return _oStudent;
+        }
+
+        // PUT api/<Student>/5
+        [HttpPut("{id}")]
+        public List<Student> Put(int id, [FromBody] Student oStudent)
+        {
+            _oStudent.RemoveAll(x => x.StudentId == oStudent.StudentId);
+            _oStudent.Add(oStudent);
+            return _oStudent;
+        }
+
+        // DELETE api/<Student>/5
+        [HttpDelete("{id}")]
+        public List<Student> Delete(int id)
+        {
+            _oStudent.RemoveAll(x => x.StudentId == id);
+            return _oStudent;
+        }
+    }
+}
