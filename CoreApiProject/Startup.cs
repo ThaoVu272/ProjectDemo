0eddc142a27389103cd8e9d7364917aae27ac5d0
Vu Thi Thao <fpt-thaovt@ssi.com.vn>
2021-08-03 14:25:16 +0700

    k

--- CoreApiProject/Startup.cs
+++ CoreApiProject/Startup.cs
@@ -26,6 +26,7 @@ namespace CoreApiProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddConsulConfig(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
